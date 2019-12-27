052fe8d75947491330e427df6145620820fab910
crissesoto <soto.crisse@hotmail.com>
2019-12-27 20:45:07 +0100

    added diffrent  routes

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ app.get("/about", function(req, res) {
   res.send("<h2>About Me</h2><p>I'm a Web developer.</p>");
 });
 
+// Services Page
+app.get("/services", function(req, res) {
+  //console.log(request);
+  res.send(
+    "<h2>My Services </h2><p>UX & UI</p><p>Web development</p><p>Prototype</p>"
+  );
+});
+
+// start server port
 app.listen(port, function() {
   console.log(`server started at poort: ${port}`);
 });
