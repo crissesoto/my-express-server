4e56aafb09b9d15c9f280169963a68d41d3ea6bc
crissesoto <soto.crisse@hotmail.com>
2019-12-27 20:28:03 +0100

    added diffrent  routes

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get("/", function(req, res) {
   res.send("<h1>Hello There!</h1>");
 });
 
+// Contact Page
+app.get("/contact", function(req, res) {
+  //console.log(request);
+  res.send("<h2>Contact me at:</h2><p>soto.crisse@hotmail.com</p>");
+});
+
+// About Me Page
+app.get("/about", function(req, res) {
+  //console.log(request);
+  res.send("<h2>About Me</h2><p>I'm a Web developer.</p>");
+});
+
 app.listen(port, function() {
   console.log(`server started at poort: ${port}`);
 });
